76cc4d75f740b6519e2fb576662d86d93b28b90f
Davi Henrique <davihareis@gmail.com>
2021-09-25 21:47:07 -0300

    adicionando animacao

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {
-  SafeAreaView,
-  Text,
-  ImageBackground,
-  TouchableOpacity
+    SafeAreaView,
+    Animated,
+    Text,
+    ImageBackground,
+    TouchableOpacity
 } from 'react-native';
 import { styles } from '../styles/styles';
 import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
     const navigation = useNavigation();
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        Animated.timing(
+            fadeAnim,
+            {
+                toValue: 1,
+                duration: 5000,
+                useNativeDriver: true
+            }
+        ).start();
+    }, [fadeAnim])
 
     return (
     <SafeAreaView style={styles.container}>
-      <ImageBackground style={styles.image} source={require('../assets/images/backgound.jpg')}>
-          <Text style={styles.title}>Star Wars</Text>
-          <Text style={styles.p}>Fonte de dados quantificada e programaticamente acessível para todos os dados do universo canônico de Star Wars!</Text>
-          <TouchableOpacity onPress={() => navigation.navigate('Filmes')}>
-              <Text style={styles.botao}>Começar</Text>
-          </TouchableOpacity>
-      </ImageBackground>
+        <Animated.View                 // Special animatable View
+            style={{
+                opacity: fadeAnim,         // Bind opacity to animated value
+            }}>
+          <ImageBackground style={styles.image} source={require('../assets/images/backgound.jpg')}>
+              <Text style={styles.title}>Star Wars</Text>
+              <Text style={styles.p}>Fonte de dados quantificada e programaticamente acessível para todos os dados do universo canônico de Star Wars!</Text>
+              <TouchableOpacity onPress={() => navigation.navigate('Filmes')}>
+                  <Text style={styles.botao}>Começar</Text>
+              </TouchableOpacity>
+          </ImageBackground>
+        </Animated.View>
     </SafeAreaView>
     );
 };
